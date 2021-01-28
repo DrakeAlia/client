a033f41dec0e3a6830b02a62dbc1d8d3c9c1e975
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-01-27 17:07:40 -0800

    coding exercise

--- src/components/App.js
+++ src/components/App.js
@@ -27,3 +27,112 @@ const App = () => {
 };
 
 export default App;
+
+
+
+// =====================================================================================================================
+// Coding Exercise 14: Adding Another Route
+
+// import React from 'react';
+// import { BrowserRouter, Route } from './react-router-dom';
+// import { Header } from './Header';
+// import { StreamList } from './StreamList';
+// import { StreamDetails } from './StreamDetails';
+
+// const App = () => {
+//     return (
+//         <div className="ui container">
+//             <BrowserRouter>
+//                 <div>
+//                     <Header />
+//                     <Route path="/" exact component={StreamList} />
+//                 </div>
+//             </BrowserRouter>
+//         </div>
+//     );
+// };
+
+// export default App;
+
+// import React from 'react';
+// import { Link } from './react-router-dom';
+
+// const StreamList = () => {
+//     return (
+//         <div className="stream-list">
+//             <h3>StreamList</h3>
+//         </div>
+//     );
+// };
+
+// export { StreamList };
+
+
+// import React from 'react';
+// import { Link } from './react-router-dom';
+
+// const StreamDetails = () => {
+//     return (
+//         <div className="stream-details">
+//             <h3>StreamDetails</h3>
+//         </div>
+//     );
+// };
+
+// export { StreamDetails };
+
+// ====================================================================================================================
+// SOLUTION:
+
+// import React from 'react';
+// import { BrowserRouter, Route } from './react-router-dom';
+// import { Header } from './Header';
+// import { StreamList } from './StreamList';
+// import { StreamDetails } from './StreamDetails';
+
+// const App = () => {
+//     return (
+//         <div className="ui container">
+//             <BrowserRouter>
+//                 <div>
+//                     <Header />
+//                     <Route path="/" exact component={StreamList} />
+//                     <Route path="/streams/details" exact component={StreamDetails} />
+//                 </div>
+//             </BrowserRouter>
+//         </div>
+//     );
+// };
+
+// export default App;
+
+// import React from 'react';
+// import { Link } from './react-router-dom';
+
+// const StreamList = () => {
+//     return (
+//         <div className="stream-list">
+//         <Link to="/streams/details" className="item">
+//            <h3>StreamList</h3>
+//            </Link>
+//         </div>
+//     );
+// };
+
+// export { StreamList };
+
+
+// import React from 'react';
+// import { Link } from './react-router-dom';
+
+// const StreamDetails = () => {
+//     return (
+//         <div className="stream-details">
+//         <Link to="/" className="item" >
+//            <h3>StreamDetails</h3>
+//            </Link>
+//         </div>
+//     );
+// };
+
+// export { StreamDetails };
